ed04e69141f758a9ba1c9513c2c419405512a012
SwastikSethi <swastiksethi7@gmail.com>
2024-06-07 18:20:22 +0530

    Utils codes for custom API response and error handling

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// app.use(cors()); // use is generally used for middlewares
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+// limit of json data accepted
+app.use(express.json({limit: "32kb"})); 
+
+// encoded url e.g space with %20%
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+
+// used to store static assets like images and pdf which are public
+app.use(express.static("public"))
+
+
+// cookies
+app.use(cookieParser())
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -9,7 +9,21 @@ dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(() =>{
+
+    app.on("error", (error)=>{
+        console.log("ERROR:" , error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port: ${process.env.PORT} `)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed   ", err);
+})
 
 
 
@@ -24,7 +38,7 @@ const app = express()
 (async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
+        
         app.on("error", (error)=>{
             console.log("ERROR:" , error);
             throw error
