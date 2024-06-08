9ed1c8e31e9caa8ae61ada49fd1f9e8a9df747d1
SwastikSethi <swastiksethi7@gmail.com>
2024-06-08 18:14:48 +0530

    added File Upload utility

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localPath) =>{
+    try {
+        if(!localPath) return null
+        
+        //upload file
+        const response = await cloudinary.uploader.upload(localPath, {
+            resource_type: "auto"
+        })
+
+        // file has been uploaded
+        console.log("File has been uploaded on cloudinary", response.url)
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(localPath); // remove the local file when upload operation failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
